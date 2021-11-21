0c9bb263354371dd10c77699c7275444ae22de8e
melody <esthermld007@gmail.com>
2021-11-20 22:53:39 -0800

    removing all the dom node that are unique to the view page

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 /* This file is where all your work should go for Assignment 2.  Please remove this comment. */
+
+function cleanUpIndex(){
+    const divContact = document.querySelector('.contact')
+    divContact.removeChild(divContact.childNodes)  
+}
+// cleanUpIndex();
+
+// function createSingleIndex(){
+//     const banner = document.querySelector()
+
+// }
+
+
+// function renderIndex(contactList){
+//     const contactList = [  
+//         { 
+//             name: "Oliver Queen", 
+//             phone: "778-555-1234", 
+//             address: "101 Main St, Star City, USA",    
+//             email: "greenarrow@watchtower.com",  
+//         },   
+//         {    
+//             name: "Jessica Cruz",    
+//             phone: "123-555-5555",    
+//             address: "Portland Oregon",    
+//             email: "greenlantern@watchtower.com",  
+//         }
+//     ]
+
+//  }
+//  function cleanUpView(){
+
+//  }
+//  function renderView(){
+     
+// }
+//  function renderView(contactList[0]){
+
+//  }
+//  function cleanUpCreate(){
+
+// }
+// function renderCreate(){
+    
+// }
+// function renderIndex(contactList[0]){
+
+// }
+
+ 
