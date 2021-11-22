7978f2ff9711a6fa410ec7c02429d48f5757d392
melody <esthermld007@gmail.com>
2021-11-21 21:14:56 -0800

    create funtion and that function will take in a single parameter

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function cleanUpIndex() {
 }
 
 function createSingleIndex(contact) {
-    return <a href="page3.html"><div class="contact">${contact.name}</div></a>
+    return '<a href="page3.html"><div class="contact">${contact.name}</div></a>'
 }
 
 function renderIndex(contactList) {
@@ -18,31 +18,33 @@ function renderIndex(contactList) {
     }
 }
 
- function renderView(){
+function cleanUpView(){
+    let classMain = document.querySelector('.form')
+    while (classMain.firstChild) {
+        classMain.removeChild(classMain.firstChild)
+    }
+}
+
+ function renderView(contactList){
     let contactType = ['name', 'email', 'phone', 'address']
     let pushedContent = []
     pushedContent.push(<div class="contact${contactType[0]}">${contact[contactType[0]]}</div>)
 
     for (let i = 1; i <= 3; i++) {
         pushedContent.push(<div class="contact${contactType[i]}">${contactType[i]}: ${contact[contactType[i]]}</div>)
-    }
-     
+    }    
 }
- function renderView(contactList[0]){
-
- }
  function cleanUpCreate(){
-    let classMain = document.querySelector('.main')
+    let classMain = document.querySelector('.contactname')
     while (classMain.firstChild) {
         classMain.removeChild(classMain.firstChild)
     }
 
 }
-// function renderCreate(){
-    
-// }
-// function renderIndex(contactList[0]){
+function renderCreate(){
+    let classMain = document.querySelector('.contactname')
+    for (let i = 0; i < contactList.length;i++) {
+        classMain.insertAdjacentHTML('beforeend', createSingleIndex(contactList[i]))
+    }
 
-// }
-
- 
+}
